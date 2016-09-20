6d3f018948896f91317174fd4259487fa1d65d30
Sam Brady <sambrady3@gmail.com>
2016-09-20 09:34:19 -0400

    Updated css and added compareu.js file with dynamic behavior for data page.

--- js/compareu.js
+++ js/compareu.js
@@ -1,3 +1,24 @@
-/**
- * Created by samuelbrady on 9/19/16.
- */
+var addedNumber = 0; //global variable keeps track of how many times a school has been added
+
+$('#schoolSelector').change(function()
+{
+    var selectedSchool = $('#schoolSelector').find(":selected").text();
+    var schoolID = selectedSchool.replace(/ /g,'');  // strip whitespace to match ID of images
+
+    // Add selected school to container, and increment the data-added-number property
+    // This code adapted from Professor's todo list example
+    $('#schoolsList').append(
+        '<div class="schoolsItem" data-added-number = ' + (addedNumber += 1) + '><span> ' + selectedSchool + '</span>' +
+        '<button onclick="deleteItem(this.parentElement)">Delete Schools</button></div>'
+    );
+
+    $('#radarTitle').html('<h2>' + selectedSchool + '</h2>');
+
+    $('.schoolPicture').hide(); //hide all images before showing selected one
+
+    $('#' + schoolID).show();
+
+});
+function deleteItem(divElem){
+    divElem.parentElement.removeChild(divElem);
+}
