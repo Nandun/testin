9dfd5000e1bcb6fc464674c24f21edbb79a17449
Sam Brady <sambrady3@gmail.com>
2016-09-25 20:56:30 -0400

    Initial setup of firebase

--- public/js/compareu.js
+++ public/js/compareu.js
@@ -24,5 +24,30 @@ function deleteItem(divElem){
 }
 
 function goBack() {
-    location.href = 'landing.html'
+    location.href = '../public/landing.html'
 }
+
+
+// this code taken from professor's example
+function show(id) {
+    $('.tab')//Select all elements with tab class
+        .removeClass('selected')//From that set, remove the class "selected" from all
+        .filter //Now filter all of the tabs by the function below
+        (function () {
+            return (this.hash === id);
+        }) //From the link that has the same # mark as what was clicked, add the selected tag
+        .addClass('selected');
+
+    $('.panel')//Select all elements with panel class
+        .hide() //Hide all of them
+        .filter(id) //Select just the one with the given id
+        .show(); //Unhide it
+}
+
+//Set a listener so that when we change the #... part of it, we call the function above
+$(window).on('hashchange', function () {
+    show(location.hash);
+});
+
+// // initialise by showing the first panel
+// show('#electrictest');
---
+++ server.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var firebase = require('firebase');
+var app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static('public'));
+
+app.post('/', function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    // DO SOMETHING HERE????
+
+});
+var port = process.env.PORT || 4000;
+
+app.listen(port, function () {
+    console.log('Example app listening on port ' + port + '!');
+});
